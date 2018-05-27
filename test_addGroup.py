1b728c9ba74bf10ccafb34a2773f1695632a4047
IrisOnika <38803895+IrisOnika@users.noreply.github.com>
2018-05-27 23:38:32 +0300

    add variables

--- test_addGroup.py
+++ test_addGroup.py
@@ -2,6 +2,13 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
 
+login='admin'
+password='secret'
+menu_tab='groups'
+group_name='testName3'
+group_logo='testLogo3'
+group_comment='comment3'
+
 
 class addGroup(unittest.TestCase):
     def setUp(self):
@@ -11,32 +18,44 @@ class addGroup(unittest.TestCase):
     def test_(self):
         success = True
         wd = self.wd
+        self.login(wd, _login=login, _password=password)
+        self.openMenu(wd, _tab=menu_tab)
+        self.groupForm(wd, _name=group_name, _logo=group_logo, _comment=group_comment)
+        self.openMenu(wd, _tab=menu_tab)
+        self.logout(wd)
+        self.assertTrue(success)
+
+    def tearDown(self):
+        self.wd.quit()
+
+    def login(self, wd, _login, _password):
         wd.get("http://127.0.0.1/addressbook/")
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(_login)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(_password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        wd.find_element_by_link_text("groups").click()
+
+    def groupForm(self, wd, _name, _logo, _comment):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("testName2")
+        wd.find_element_by_name("group_name").send_keys(_name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("testLogo2")
+        wd.find_element_by_name("group_header").send_keys(_logo)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("testComment2")
+        wd.find_element_by_name("group_footer").send_keys(_comment)
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("groups").click()
+
+    def openMenu(self, wd, _tab):
+        wd.find_element_by_link_text(_tab).click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
-        self.assertTrue(success)
-    
-    def tearDown(self):
-        self.wd.quit()
 
 
 if __name__ == '__main__':
