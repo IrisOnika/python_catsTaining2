74d69fa18243b59309cfeb3ad05791ffe0b15feb
IrisOnika <38803895+IrisOnika@users.noreply.github.com>
2018-05-28 00:20:34 +0300

    separate group class

---
+++ groupForm.py
@@ -0,0 +1,9 @@
+
+
+class group():
+
+    def __init__(self,  _name, _logo, _comment):
+        self.name=_name
+        self.logo = _logo
+        self.comment = _comment
+
--- test_addGroup.py
+++ test_addGroup.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+from groupForm import group
 
 login='admin'
 password='secret'
@@ -18,9 +19,10 @@ class addGroup(unittest.TestCase):
     def test_(self):
         success = True
         wd = self.wd
+        self.openStartPage(wd)
         self.login(wd, _login=login, _password=password)
         self.openMenu(wd, _tab=menu_tab)
-        self.groupForm(wd, _name=group_name, _logo=group_logo, _comment=group_comment)
+        self.groupForm(wd, group(_name=group_name, _logo=group_logo, _comment=group_comment))
         self.openMenu(wd, _tab=menu_tab)
         self.logout(wd)
         self.assertTrue(success)
@@ -29,7 +31,6 @@ class addGroup(unittest.TestCase):
         self.wd.quit()
 
     def login(self, wd, _login, _password):
-        wd.get("http://127.0.0.1/addressbook/")
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(_login)
@@ -38,17 +39,20 @@ class addGroup(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(_password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
-    def groupForm(self, wd, _name, _logo, _comment):
+    def openStartPage(self, wd):
+        wd.get("http://127.0.0.1/addressbook/")
+
+    def groupForm(self, wd, group):
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(_name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(_logo)
+        wd.find_element_by_name("group_header").send_keys(group.logo)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(_comment)
+        wd.find_element_by_name("group_footer").send_keys(group.comment)
         wd.find_element_by_name("submit").click()
 
     def openMenu(self, wd, _tab):
