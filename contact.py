0589dc75ae61fe48ffa8d9c759c5f85bdf78c652
IrisOnika <38803895+IrisOnika@users.noreply.github.com>
2018-05-31 00:38:27 +0300

    add cotact file

---
+++ contact.py
@@ -0,0 +1,30 @@
+
+
+class Group():
+
+    def __init__(self,  _firstname, _middlename, _lastname, _nickname, _title, _company, _address, _thome, _tmobile, _
+        twork, _tfax, _email, _email2, _email3, _homepage, _bday, _bmonth, _byear,
+                 _aday, _amonth, _ayear, _address2, _phone2, _notes):
+        self.firstname=_firstname
+        self.middlename = _middlename
+        self.lastname = _lastname
+        self.nickname = _nickname
+        self.title = _title
+        self.company = _company
+        self.address = _address
+        self.thome = _thome
+        self.tmobile = _tmobile
+        self.twork = _twork
+        self.tfax = _tfax_
+        self.email = _email
+        self.email2 = _email2
+        self.email3 = _email3
+        self.homepage = _homepage
+        self.bmonth = _bmonth
+        self.byear = _byear
+        self.aday = _aday
+        self.amonth = _amonth
+        self.ayear = _ayear
+        self.address2 = _address2
+        self.phone2 = _phone2
+        self.notes = _notes
