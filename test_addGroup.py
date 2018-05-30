90925069c62460cceee9ffa75db156a2dd959517
IrisOnika <38803895+IrisOnika@users.noreply.github.com>
2018-05-30 23:28:51 +0300

    delete nop used code, move some small methods to group and login methods optimized xpath of login button

--- test_addGroup.py
+++ test_addGroup.py
@@ -17,32 +17,30 @@ class addGroup(unittest.TestCase):
         self.wd.implicitly_wait(60)
     
     def test_(self):
-        success = True
         wd = self.wd
-        self.openStartPage(wd)
         self.login(wd, _login=login, _password=password)
-        self.openMenu(wd, _tab=menu_tab)
         self.groupForm(wd, Group(_name=group_name, _logo=group_logo, _comment=group_comment))
-        self.openMenu(wd, _tab=menu_tab)
         self.logout(wd)
-        self.assertTrue(success)
 
     def tearDown(self):
         self.wd.quit()
 
     def login(self, wd, _login, _password):
+        self.openStartPage(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(_login)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(_password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[@type='submit']").click()
 
     def openStartPage(self, wd):
         wd.get("http://127.0.0.1/addressbook/")
 
     def groupForm(self, wd, Group):
+        menu_tab = 'groups'
+        self.openMenu(wd, _tab=menu_tab)
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -54,6 +52,7 @@ class addGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(Group.comment)
         wd.find_element_by_name("submit").click()
+        self.openMenu(wd, _tab=menu_tab)
 
     def openMenu(self, wd, _tab):
         wd.find_element_by_link_text(_tab).click()
