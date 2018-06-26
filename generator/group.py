6274f405e5141531fd026e86a4386973418c1a5c
IrisOnika <38803895+IrisOnika@users.noreply.github.com>
2018-06-26 23:51:53 +0300

    add test data generator for groups (with json pack)

---
+++ generator/group.py
@@ -0,0 +1,44 @@
+from model.group import Group
+import random
+import string
+import os.path
+import json
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/groups.json"
+
+for o, a in opts:    #o - is a name of option; a - is its option value
+    if o == '-n':
+        n = int(a)   # if option is number of groups - convert it to int
+    elif o == '-f':
+        f = a
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " " * 11
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+test_data = [Group(_name='',
+                   _logo='',
+                   _comment='')] + [
+             Group(_name=random_string('Name', 11),
+                   _logo=random_string('Logo', 22),
+                   _comment=random_string('Comment', 44))
+             for i in range(n)
+             ]
+
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, 'w') as f_data:
+    f_data.write(json.dumps(test_data, default=lambda x: x.__dict__, indent=2))
