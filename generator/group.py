cef08003700589018c52efe0e7dfb13ddd9ddfd0
IrisOnika <38803895+IrisOnika@users.noreply.github.com>
2018-06-27 00:07:00 +0300

    the same generator with jsonpickle

--- generator/group.py
+++ generator/group.py
@@ -2,7 +2,8 @@ from model.group import Group
 import random
 import string
 import os.path
-import json
+#import json
+import jsonpickle
 import getopt
 import sys
 
@@ -41,4 +42,6 @@ test_data = [Group(_name='',
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, 'w') as f_data:
-    f_data.write(json.dumps(test_data, default=lambda x: x.__dict__, indent=2))
+    #f_data.write(json.dumps(test_data, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    f_data.write(jsonpickle.encode(test_data))
