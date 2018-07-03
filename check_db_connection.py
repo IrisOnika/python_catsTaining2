81a39bf2bd3f01b1ae1417ba0d86bca93fc37923
IrisOnika <38803895+IrisOnika@users.noreply.github.com>
2018-07-03 22:41:03 +0300

    add orm fixture

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,11 +1,33 @@
-import pymysql.cursors
+#import pymysql.cursors
+from fixture.orm import ORMFixture
 
-connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+
+#connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+db = ORMFixture(_host="127.0.0.1", _database="addressbook", _user="root", _password="")
 
 try:
-    cursor = connection.cursor()
-    cursor.execute("select * from group_list")
-    for row in cursor.fetchall():              #fetchall returns all info as list of rows(like in db)
-        print(row)
+    list = db.orm_get_contact_list()
+    for item in list:
+        print(item)
+    print(len(list))
 finally:
-    connection.close()
+    pass #connection.close()
+
+
+
+
+
+
+
+
+
+
+
+
+#try:
+#    cursor = connection.cursor()
+#    cursor.execute("select * from group_list")
+#    for row in cursor.fetchall():              #fetchall returns all info as list of rows(like in db)
+#        print(row)
+#finally:
+#    connection.close()
