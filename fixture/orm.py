0189c594dcd4eeba329f8b7b53c01bb19802d9cd
IrisOnika <38803895+IrisOnika@users.noreply.github.com>
2018-07-04 21:05:32 +0300

    add relation to fixture

--- fixture/orm.py
+++ fixture/orm.py
@@ -15,6 +15,12 @@ class ORMFixture:
         name = Optional(str, column='group_name')
         logo = Optional(str, column='group_header')
         comment = Optional(str, column='group_footer')
+        contacts = Set(
+            lambda: ORMFixture.ORMContact,
+            table="address_in_groups",
+            column="id",
+            reverse="groups",
+            lazy=True)
 
     class ORMContact(db.Entity):
         _table_ = 'addressbook'
@@ -22,6 +28,12 @@ class ORMFixture:
         firstname = Optional(str, column='firstname')
         lastname = Optional(str, column='lastname')
         deprecated = Optional(str, column='deprecated')
+        groups = Set(
+            lambda: ORMFixture.ORMGroup,
+            table="address_in_groups",
+            column="group_id",
+            reverse="contacts",
+            lazy=True)
 
     def __init__(self, _host, _database, _user, _password):
         conv = encoders
@@ -49,5 +61,13 @@ class ORMFixture:
             return Contact(_id=str(contact.id), _firstname=contact.firstname, _lastname=contact.lastname)
         return list(map(convert, contacts))
 
+    @db_session
+    def get_contacts_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_to_model_contact(orm_group.contacts)
 
-
+    @db_session
+    def get_contacts_out_of_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]
+        return self.convert_to_model_contact(
+            select(c for c in ORMFixture.ORMContact if c.deprecated is None and orm_group not in c.groups))
