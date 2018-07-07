c2ac07cd12256fd4c73fb9bd276a403391abca14
IrisOnika <38803895+IrisOnika@users.noreply.github.com>
2018-07-07 09:39:15 +0300

    orm is not finded as fixture

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,12 +1,13 @@
 #import pymysql.cursors
 from fixture.orm import ORMFixture
+from model.group import Group
 
 
 #connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
 db = ORMFixture(_host="127.0.0.1", _database="addressbook", _user="root", _password="")
 
 try:
-    list = db.orm_get_contact_list()
+    list = db.get_contacts_out_of_group(Group(_id=300))
     for item in list:
         print(item)
     print(len(list))
